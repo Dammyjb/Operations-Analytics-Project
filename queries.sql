2cb58cc092a0d82e0d594f8213b1f228c38292e5
Dammyjb <ayodejiogunbiyi@gmail.com>
2026-06-10 12:00:31 -0400

    Completed Queries

--- queries.sql
+++ queries.sql
@@ -37,24 +37,50 @@ ORDER BY ExcessUnits DESC;
 
 --Revenue insights
     --Revenue by warehouse
-    --Revenue by product category
+SELECT w.WarehouseName, ROUND(SUM(Revenue),2) AS WarehouseRevenue
+FROM Orders o
+JOIN Warehouses w
+ON o.WarehouseID = w.WarehouseID
+GROUP BY w.WarehouseName
+ORDER BY WarehouseRevenue DESC;
 
+    --Revenue by product category
+SELECT p.Category, ROUND(SUM(Revenue),3) as TotalRev
+FROM Orders o
+JOIN Products p
+ON p.ProductID = o.ProductID
+GROUP BY p.Category
+ORDER BY TotalRev DESC;
 
 
 --Efficiency
     --Average delivery delay by region
+SELECT CustomerRegion, AVG(DATEDIFF(day, DeliveryDate,ActualDeliveryDate)) AS AvgDelay
+FROM Orders
+WHERE ActualDeliveryDate > DeliveryDate
+GROUP BY CustomerRegion;
+
     --On-time vs late delivery rate
 
+SELECT p.Category,CAST(ROUND(SUM(
+        CASE 
+            WHEN ActualDeliveryDate <= DeliveryDate THEN 1
+            ELSE 0
+            END) * 100.0 / COUNT(CASE 
+            WHEN ActualDeliveryDate <= DeliveryDate THEN 1
+            ELSE 0
+            END),2) AS decimal(5,2)) AS EarlyRatePercent, 
+        CAST(ROUND((Count(p.Category) - SUM(
+            CASE 
+            WHEN ActualDeliveryDate <= DeliveryDate THEN 1
+            ELSE 0
+            END))* 100.0/ COUNT(CASE 
+            WHEN ActualDeliveryDate <= DeliveryDate THEN 1
+            ELSE 0
+            END), 2) AS decimal (5,2)) AS LateRatePercent
+FROM Orders o
+JOIN Products p
+ON o.ProductID = p.ProductID
+GROUP BY p.Category;
 
 
-
---Operations
-    --Top 10 fastest moving products
-    --Employees per warehouse and workload distribution
-
-
-
---Employee Insights
-    --Employees per warehouse
-    --Manager-to-worker ratios
-    --Warehouses with the largest workforce
